26499a6f09767af92a2751830383d342b96e9565
sunhara <Tom_gaoshen@163.com>
2023-09-25 15:08:36 +0800

    First push

---
+++ Advance Steel 2023 Solution/Plugin.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.AdvanceSteel.Runtime;
+
+namespace HelloWorld
+{
+    public sealed class Plugin : IExtensionApplication
+
+    {
+        void IExtensionApplication.Initialize()
+        {
+        }
+        void IExtensionApplication.Terminate()
+        {
+        }
+    }
+}
---
+++ Advance Steel 2023 Solution/TestClass.cs
@@ -0,0 +1,16 @@
+﻿using Autodesk.AdvanceSteel.CADAccess;
+using Autodesk.AdvanceSteel.DocumentManagement;
+using Autodesk.AdvanceSteel.Runtime;
+using System.Windows.Forms;
+
+namespace HelloWorld
+{
+    public class TestClass
+    {
+        [CommandMethodAttribute("TEST_GROUP", "HelloWorld", "HelloWorld", CommandFlags.Modal)]
+        public void SayHelloWorld()
+        {
+            MessageBox.Show("Hello World!");
+        }
+    }
+}
